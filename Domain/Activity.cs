e3958b8c2a8d6eb6c189e4e31432cc70801f23ac
Oh Dylan <ddragon1128@hotmail.com>
2021-04-19 21:17:53 +0800

    Initial commit, database migration done

---
+++ Domain/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Activity
+    {
+        // Entity framwork is going to recognize this to be the primary key when Activities table created
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // The name of this property is going to map to table name in db, e.g. Activities
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
