8c3bab9dd774e469955c9c2b151ec91b8dc37a6e
Oh Dylan <ddragon1128@hotmail.com>
2021-07-19 17:42:25 +0800

    Handle relationships of Host, attendees and activities

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Application.Core
@@ -12,7 +13,13 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
-            CreateMap<Activity, ActivityDto>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
+                    .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
         }
     }
 }
